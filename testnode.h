7d2af1ddf2c9aa415d4e040487dc98f7164830dc
Sam Jo <123586891+samlora704@users.noreply.github.com>
2024-12-18 09:16:56 -0500

    testnode.h

---
+++ testnode.h
@@ -0,0 +1,35 @@
+what is the primary advantage of a dynamic array over a static array 
+  -A. the ability to resize itself at runtime.
+what is the primary purpose of using class templates in c++?
+  -A.act as a blueprint to create objects with similar properties.
+protected members can be accesssed from what classes?
+  -A derived (subclass) classes
+A map allow us to store elements so they can be located quickly using?
+-A keys. It functions as an associative container, storing elements in key-value pairs.
+what is an abstract class in c++?
+  -  a class that is designed to be specifically used as a base class
+a friend ___can access private and protected members of another class in which is declared
+  -a. friend classes and functions in
+a pure virtual is created  by giving a ___of the body of the function
+     -a. zero
+a binary tree is an ordered tree in which every node has at most __ children?
+    -a 2
+____is a collection of nodes that together form a linear ordering
+    -a linked list 
+what is encapsulation?
+    -a.  a fundamental concept in object-oriented programming (OOP) that involves bundling data and methods into a single unit, or object
+A ___- is a data structure that stores any collection of elements
+     -a Array 
+___ allows for multiple entries to have the same key
+    -a. map 
+in c++ what does the stream insertion operator (<<) typcially overload for user defined classes?
+    - A. when overloaded for user-defined classes is typically used to enable the printing of objects of that class directly to an output stream (like cout), allowing for a more intuitive and readable way to display the object's data
+AN ___IS A MATHEMATICAL MODEL OF A DATA STRUCTRUE THAT SPECIFIES THE TYPE OF DATA STORED SUPOORT ON THEM AND THE TYPES OF PARAMETERS OF THE OPERATION 
+    -A. An Abstract Data Type (ADT)
+AN OBJECT IS ___ OF A CLASS
+    -A. AN INSTANCE 
+A stak is a container of objects that are inserted and removed according to the ___ prionciple
+    -a. Last in first out (LIFO)
+Pointers are variables that store the __of another variable 
+    a. memory address
+
